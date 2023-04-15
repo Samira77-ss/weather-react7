11d2945e9a3478551baebefcee505ef7d0159b02
Samira77-ss <samiraseyfii0709@gmail.com>
2023-04-15 11:14:50 +0330

    Added initial changes

---
+++ src/Weather.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
+import "./Weather.css";
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = "094780c710fa4efd669f0df8c3991927";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <div className="Weather">
+      <h1>Hello React</h1>
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+    </div>
+  );
+}
